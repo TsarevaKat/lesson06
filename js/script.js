44adeca3116d74ab8eb3f862b84d5f669f32a6f6
Kate Tsareva <katetsareva3012@mail.ru>
2020-01-31 00:01:39 +0300

    feature: lesson06 hard | script

--- js/script.js
+++ js/script.js
@@ -5,26 +5,40 @@ const isNumber = function (n) {
 };
 
 function bot() {
-  const num = 77;
-  function botGame() {
-    const numUser = prompt('Угадай чисто от 1 до 100');
-    if (numUser === null) {
-      return 0;
-    }
-    console.log('numUser: ', numUser);
+  const num = Math.floor(1 - 0.5 + Math.random() * 100);
+  // console.log('num: ', num);
 
-    // numUser += numUser;
-    if (!isNumber(numUser)) {
-      alert('Это не число! Введи число!');
-      botGame();
-    } else
-    if (numUser > num) {
-      alert('Загаданное число меньше');
-      botGame();
-    } else
-    if (numUser < num) {
-      alert('Загаданное число больше');
-      botGame();
+  let index = 10;
+  function botGame() {
+    if (index > 0) {
+      const numUser = prompt('Угадай чисто от 1 до 100');
+      if (numUser === null) {
+        alert('Пока :(');
+        return 0;
+      }
+      if (!isNumber(numUser)) {
+        alert('Это не число! Введи число!');
+        botGame();
+      } else if (numUser > num) {
+        index--;
+        alert(`Загаданное число меньше, осталось попыток ${index}`);
+        botGame();
+      } else if (numUser < num) {
+        index--;
+        alert(`Загаданное число больше, осталось попыток ${index}`);
+        botGame();
+      } else if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+          bot();
+        } else {
+          alert('Пока :(');
+          return 0;
+        }
+
+    } else if (confirm('Попытки закончились, хотите сыграть еще?')) {
+      bot();
+    } else {
+      alert('Пока :(');
+      return 0;
     }
   }
   botGame();
